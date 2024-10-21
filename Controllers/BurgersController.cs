bc6880f9c988fa6766b5a52e4840ec8cb76bb4a9
Mundrack <mateopuga75@gmail.com>
2024-10-21 10:35:06 -0500

    ordenamos las cosas en sus lugares

--- Controllers/BurgersController.cs
+++ Controllers/BurgersController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MateoPugaProyectoII.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
-namespace MateoPugaProyectoII.Models
+namespace MateoPugaProyectoII.Controllers
 {
     public class BurgersController : Controller
     {
